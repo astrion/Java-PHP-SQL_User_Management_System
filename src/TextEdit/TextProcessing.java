de58127abd35c9c32611db4fe85f215a3e9a1327
mooid <moooosee@outlook.com>
2021-04-11 12:10:37 -0400

    issue #5: add clear() method for cleaning last edits, refactor (stabilize poc code), add classes for unit tests

--- src/TextEdit/TextProcessing.java
+++ src/TextEdit/TextProcessing.java
@@ -2,26 +2,30 @@ package TextEdit;
 
 public interface TextProcessing {
 
-    default public String findChange(String prev, String next) {
+    /**
+     *  find the difference (a single substring) between two strings
+     * */
+    default public String findDifference(String prev, String next) {
 
+        // set the shortest string as s1, the longest string as s2
         String s1 = prev, s2 = next;
-        int start = -1, end = -1;
-
         if (prev.length() > next.length()) {
             s1 = next;
             s2 = prev;
         }
 
+        // calculate the start and end indexes of the difference between s1 and s2
+        int start = -1, end = -1;
         if (s2.startsWith(s1)) {
-            // added to the right extreme
+            // if a single difference (a single substring) exists in s2's right extreme
             start = s1.length();
             end = s2.length();
         } else if (s2.endsWith(s1)) {
-            // added to the left extreme
+            // if a single difference (a single substring) exists in s2's left extreme
             start = 0;
             end = s2.length() - s1.length();
         } else {
-            // middle
+            // if a single difference (a single substring) exists in s2's middle
             for (int c = 0; c < s2.length(); c++) {
                 try {
                     if ( start < 0 & prev.charAt(c) != next.charAt(c) ) start = c;
@@ -31,10 +35,7 @@ public interface TextProcessing {
             }
         }
 
-        String diff = "";
-        diff = s2.substring(start, end);
-
-        return diff;
+        return s2.substring(start, end);
     }
 
 }
