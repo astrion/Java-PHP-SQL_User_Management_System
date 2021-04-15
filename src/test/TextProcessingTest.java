628a492beb6649bd6343b20478e6a750d60518d0
mooid <moooosee@outlook.com>
2021-04-15 18:53:58 -0400

    issue #8: add unit tests (at EnumCommandCaptionTest.java, EnumWindowCaptionTest.java, SmartUndoManagerTest.java, TextProcessingTest.java)

--- src/test/TextProcessingTest.java
+++ src/test/TextProcessingTest.java
@@ -10,22 +10,28 @@ class TextProcessingObject implements TextProcessing {}
 
 public class TextProcessingTest {
 
+    /**
+     * assert 1 difference is returned after 1 sub-string addition
+     * */
     @Test
     void test_findChange_for_1_addition() {
         TextProcessingObject o = new TextProcessingObject();
 
-        assertEquals(" World!", o.findDifference("Hello", "Hello World!"));
-        assertEquals("Hello ", o.findDifference("World!", "Hello World!"));
-        assertEquals(" World", o.findDifference("Hello!", "Hello World!"));
+        assertEquals(" World!", o.findDifference("Hello", "Hello World!")); // added suffix
+        assertEquals("Hello ", o.findDifference("World!", "Hello World!")); // added prefix
+        assertEquals(" World", o.findDifference("Hello!", "Hello World!")); // added mid-string
     }
 
+    /**
+     * assert 1 difference is returned after an 1 sub-string removal
+     * */
     @Test
     void test_findChange_for_1_removal() {
         TextProcessingObject o = new TextProcessingObject();
 
-        assertEquals(" World!", o.findDifference("Hello World!", "Hello"));
-        assertEquals("Hello ", o.findDifference("Hello World!", "World!"));
-        assertEquals(" World", o.findDifference("Hello World!", "Hello!"));
+        assertEquals(" World!", o.findDifference("Hello World!", "Hello")); // removed suffix
+        assertEquals("Hello ", o.findDifference("Hello World!", "World!")); // removed prefix
+        assertEquals(" World", o.findDifference("Hello World!", "Hello!")); // removed mid-string
     }
 
 }
