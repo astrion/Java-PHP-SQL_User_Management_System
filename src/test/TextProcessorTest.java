72da2bb830de6c0bc21e5e3b08917ca4617fb27c
mooid <moooosee@outlook.com>
2021-04-19 17:38:10 -0400

    add test/TextProcessorTest.java

--- src/test/TextProcessorTest.java
+++ src/test/TextProcessorTest.java
@@ -1,4 +1,16 @@
 package test;
 
+import TextEdit.TextProcessor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TextProcessorTest {
+
+    @Test
+    void test_TextProcessor_TextProcessor() {
+        TextProcessor textProcessor = new TextProcessor();
+        assertTrue(textProcessor != null);
+    }
+
 }
