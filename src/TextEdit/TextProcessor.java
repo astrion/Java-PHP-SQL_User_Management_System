aa25798715e2ea11f4ff24f9946d83652e16fb4f
hyun <astrionhyun@gmail.com>
2021-04-20 15:55:11 -0400

    updated code comments

--- src/TextEdit/TextProcessor.java
+++ src/TextEdit/TextProcessor.java
@@ -56,7 +56,9 @@ public class TextProcessor {
 
         // insert/extract the selection information into ArrayList
         ArrayList<Object[]> pairList = new ArrayList<Object[]>();
+        // obtain a model of the table to access its entries
         DefaultTableModel model = dataTable.getModel();
+        // iterate through entries of the table from the beginning to the end
         for (int i = 0; i < model.getRowCount(); i++) {
             if (model.getValueAt(i, Column.Select.Index()).equals(true)) {
                 // line number, state number, row number to delete
@@ -71,26 +73,34 @@ public class TextProcessor {
 
         // delete unnecessary entries
         for (int i = pairList.size() - 1; i >= 0; --i) {
+            // line state pair is used as a container to store delete target line, state, row number (unused) info
             Object[] lineStatePair = pairList.get(i);
             int lineNum = (int) lineStatePair[0];
             int stateNum = (int) lineStatePair[1];
+            // obtain the latest state within the given line number
             int latestState = (int) historyManager.get(lineNum).size() - 1;
+            // from the latest state, it loops through the states until it reach the target state
+            // thus, all newer states are deleted from the data structure
             for (int j = latestState; j > stateNum; --j) {
                 historyManager.get(lineNum).remove(j);
             }
-            //decrease line number
-            if ((stateNum == 0) && historyManager.get(lineNum).get(stateNum).equals("") && lineNum>0) {
+            // when (1)the state number is 0  (2)the line has a blank state (3)the line number is bigger than 0
+            // then we remove the line so that there is no gap among lines  (eg. line 0,2,3 -> line 0,1,2)
+            if ((stateNum == 0) && historyManager.get(lineNum).get(stateNum).equals("") && lineNum > 0) {
                 historyManager.remove(lineNum);
             }
         }
 
         // update JTextarea with the lines with the latest texts
-        ArrayList<String> s = new ArrayList<>();
+        ArrayList<String> tempStringArray = new ArrayList<>();
+        // loop through the data structure to update JTextArea
         for (int i = 0; i < historyManager.size(); i++) {
-            s.add(historyManager.get(i).get(historyManager.get(i).size() - 1));
+            // tempStringArray has following structure (eg. size[2][1]: ["apple"]["banana"])
+            tempStringArray.add(historyManager.get(i).get(historyManager.get(i).size() - 1));
         }
-
-        String[] lineSeparateArray = s.toArray(new String[s.size()]);
+        // convert the above to (eg. size[2]: ["apple","banana"])
+        String[] lineSeparateArray = tempStringArray.toArray(new String[tempStringArray.size()]);
+        // convert the above to (eg. "apple\nbanana")
         String joined = String.join("\n", lineSeparateArray);
         area.setText(joined);
 
@@ -98,14 +108,20 @@ public class TextProcessor {
         dataTable.Refresh(historyManager);
     }
 
+
+    // perform a forget function
     public static void Forget(List<List<String>> historyManager, TextEditTable dataTable) {
 
         // insert/extract the selection information into ArrayList
         ArrayList<Object[]> pairList = new ArrayList<Object[]>();
+        // obtain a model of the table to access its entries
         DefaultTableModel model = dataTable.getModel();
+
+        // iterate through entries of the table from the beginning to the end
         for (int i = 0; i < model.getRowCount(); i++) {
+            // if the current row is selected then
             if (model.getValueAt(i, Column.Select.Index()).equals(true)) {
-                // line number, state number, row number to delete
+                // obtain line number, state number, row number to delete
                 pairList.add(new Object[]{
                         model.getValueAt(i, Column.Line.Index()),
                         model.getValueAt(i, Column.State.Index()),
@@ -115,30 +131,42 @@ public class TextProcessor {
         }
 
         // delete unnecessary entries
+        // iteration starts from the latest delete items - this can be start from 0, but it happened to be this way as
+        // this section of code is re-used from undo part.
         for (int i = pairList.size() - 1; i >= 0; --i) {
             Object[] lineStatePair = pairList.get(i);
             int lineNum = (int) lineStatePair[0];
             int stateNum = (int) lineStatePair[1];
             int oldestState = 0;
-
+            // starting from the oldest state, it deletes record one by one to the target state. It stops before the
+            // target entry so that the current selection history is preserved
             for (int j = oldestState; j < stateNum; j++) {
                 historyManager.get(lineNum).remove(0);
             }
-            // decrease state number
-
         }
         //data refresh
         dataTable.Refresh(historyManager);
     }
 
+    // select latest entries of radio buttons so that forget function can use the selections
+    // a small trick is used here. Due to selection implementation, all the non-selected entries in the same line
+    // is deselected. Thus, selecting from the state 0 to the state n will only leave the selection of state n.
+    // using this trick, the software can obtain the latest selections for each lines. (eg. line 0 state n, line1 state n)
     public static void SelectLatest(TextEditTable dataTable) {
+        // obtain a model of the table to access its entries
         DefaultTableModel model = dataTable.getModel();
+        // starting from the first entry, it approaches to
         for (int i = 0; i < model.getRowCount(); i++) {
             dataTable.setValueAt(true, i, Column.Select.Index());
         }
     }
 
+    // select oldest entries of radio buttons so that undo function can use the selections
+    // a small trick is used here. Due to selection implementation, all the non-selected entries in the same line
+    // is deselected. Thus, selecting from the state n to the state 0 will only leave the selection of state 0.
+    // using this trick, the software can obtain the oldest selections for each lines. (eg. line0 state0, line1 state 0)
     public static void SelectOldest(TextEditTable dataTable) {
+        // obtain a model of the table to access its entries
         DefaultTableModel model = dataTable.getModel();
         for (int i = model.getRowCount() - 1; i >= 0; --i) {
             dataTable.setValueAt(true, i, Column.Select.Index());
