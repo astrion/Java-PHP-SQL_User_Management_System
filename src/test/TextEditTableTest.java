f3ab1a4185722cc6189a34b9749dc8a15d101e1a
mooid <moooosee@outlook.com>
2021-04-20 11:17:22 -0400

    add tests to src/test/TextEditTableTest.java, src/test/TextProcessorTest.java

--- src/test/TextEditTableTest.java
+++ src/test/TextEditTableTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import TextEdit.TextEditTable;
+import TextEdit.TextProcessor;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -8,7 +9,7 @@ import javax.swing.table.DefaultTableModel;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TextEditTableTest {
 
@@ -26,24 +27,75 @@ public class TextEditTableTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void test_TextEditTable_columnClass(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Object[][] rowData = new Object[i][j];
+                TextEditTable textEditTable = new TextEditTable(rowData);
+
+                Class<?> actualColumnClass = textEditTable.getColumnClass(n);
+                switch (n) {
+                    case 0:
+                        assertEquals(Integer.class, actualColumnClass);
+                        break;
+                    case 1:
+                        assertEquals(Integer.class, actualColumnClass);
+                        break;
+                    case 3:
+                        assertEquals(Boolean.class, actualColumnClass);
+                        break;
+                    default:
+                        assertEquals(String.class, actualColumnClass);
+
+                }
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
     void test_TextEditTable_getModel(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 Object[][] rowData = new Object[i][j];
                 TextEditTable textEditTable = new TextEditTable(rowData);
 
-                boolean isMethodExecutedWithoutInterruptions;
-                try {
-                    DefaultTableModel defaultTableModel = textEditTable.getModel();
-
-                    isMethodExecutedWithoutInterruptions = true;
-                } catch (Exception e) {
-                    isMethodExecutedWithoutInterruptions = false;
-                }
-                assertTrue(isMethodExecutedWithoutInterruptions);
+                assertAll(() -> textEditTable.getModel());
             }
         }
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void test_TextEditTable_newColumns(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Object[][] rowData = new Object[i][j];
+                TextEditTable textEditTable = new TextEditTable(rowData);
+
+                final int index = n; // Note: this is unused in newColumns(), index is kept in this test for exploring method behaviour
+                final int line = i;
+                final int state = j;
+                assertAll(() -> textEditTable.newColumns(n, "some text here...", line, state));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1})
+    void test_TextEditTable_addRow(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Object[][] rowData = new Object[i][j];
+                TextEditTable textEditTable = new TextEditTable(rowData);
+
+                final int line = i;
+                final int state = j;
+                assertAll(() -> textEditTable.addRow("some text here...", line, state));
+            }
+        }
+    }
+
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
@@ -60,15 +112,7 @@ public class TextEditTableTest {
                 Object[][] rowData = new Object[i][j];
                 TextEditTable textEditTable = new TextEditTable(rowData);
 
-                boolean isMethodExecutedWithoutInterruptions;
-                try {
-                    textEditTable.Refresh(editHistory);
-
-                    isMethodExecutedWithoutInterruptions = true;
-                } catch (Exception e) {
-                    isMethodExecutedWithoutInterruptions = false;
-                }
-                assertTrue(isMethodExecutedWithoutInterruptions);
+                assertAll(() -> textEditTable.Refresh(editHistory));
             }
         }
     }
